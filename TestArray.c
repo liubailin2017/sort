3f1ef08660deb4b66dc57bdd57a1ce03e5d4658d
liubailin <liubailinprivate@163.com>
2019-05-16 10:48:16 +0800

    add .gitignore

--- TestArray.c
+++ TestArray.c
@@ -2,12 +2,12 @@
 #include"BubbleSort.c"
 #include"QuickSort.c"
 #include"InsertSort.c"
-int Resouce[] = {13,12,1,100,99,101,10,123,1,99};
+int Resouce[] = {10,9,8,7,6,5,4,3,2,1,};
 int N = 10; 
 
 int main()
 {
-	printf("%d\n",InsertSort(Resouce,10));
+	printf("%d\n",BubbleSort(Resouce,10));
 	for(int i = 0; i < N; i++){
 		printf("%d,",Resouce[i]);
 	}
